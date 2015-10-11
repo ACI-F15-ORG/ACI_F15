68eb3233ee10f8ae59c1ec8a989c1ade3fc0f4b5
Shawn Lawson <lawsos2@rpi.edu>
2015-10-11 12:52:35 -0400

    commented in class code

--- InClassCode/Oct9_Slit_Scanning/ofApp.cpp
+++ InClassCode/Oct9_Slit_Scanning/ofApp.cpp
@@ -5,27 +5,42 @@ void ofApp::setup(){
     camWidth = 320;
     camHeight = 240;
     
+    // setup properties for our camera
     vidGrabber.setVerbose(true);
     vidGrabber.setDeviceID(0);
     vidGrabber.setDesiredFrameRate(30);
     vidGrabber.initGrabber(camWidth, camHeight);
     
+    //allocate memory for our output image
     vidPixels.allocate(camWidth, camHeight, OF_PIXELS_RGB);
+    //tell the texture how to allocate based on our pixel information
     vidTexture.allocate(vidPixels);
+
+    //if you're on windows you may need to specify the OF framerate
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //must update the video camera
     vidGrabber.update();
     
+    //if we don't check for is frame new, then because our OF framerate
+    //doesn't match our camera framerate, then the drawn columns look 
+    //wide
     if(vidGrabber.isFrameNew()){
+        //get a handle to the raw pixels from the camera
         ofPixelsRef pixelRef = vidGrabber.getPixelsRef();
+        //make sure we loop around the width of the image
         column  = (column + 1) % camWidth;
         for (int i = 0; i < camHeight; i++) {
             vidPixels.setColor(column,
                                i,
                                pixelRef.getColor(column, i));
+                                //swap column for 160 to change style of 
+                                //slit scanning
+
         }
+        //tell texture to update with our newest pixel data
         vidTexture.loadData(vidPixels);
     }
 }
